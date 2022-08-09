28c6bbd7a2e9c0a835cf4bc0139d205432840f4c
PritenMakwana44 <priten.makwana7@gmail.com>
2022-08-09 14:11:35 +0000

    display addition question to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,19 +12,30 @@ document.addEventListener("DOMContentLoaded", function() {
             else {
                 let gameType = this.getAttribute("data-type");
                 alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
 })
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed.
  */
-function runGame() {
+function runGame(gameType) {
     // Creates two random number between 1 and 25
 
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert (`Unknown game type: ${gameType}`);
+        throw `Unknown hame type: ${gameType}. Aborting!`;
+    }
 
 }
 
@@ -44,7 +55,10 @@ function incrementWrongAnswer(){
 
 }
 
-function displayAdditionQuestion(){
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 
 }
 
