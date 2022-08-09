5e0cc6d21788bcf5bafe1f2d145ead73da704c15
PritenMakwana44 <priten.makwana7@gmail.com>
2022-08-09 15:37:52 +0000

    Add multiply code in all 3 functions needed

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,10 +32,17 @@ function runGame(gameType) {
     if (gameType === "addition") {
 
         displayAdditionQuestion(num1, num2);
+
+    } else if (gameType === "multiply") {
+
+        displayMultiplyQuestion(num1, num2);
+
     } else {
         alert (`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
+
+
 
 }
 
@@ -73,13 +80,15 @@ function calculateCorrectAnswer(){
 
     if(operator === '+') {
         return [operand1 + operand2, "addition"];
+    } else if (operator === 'x') {
+        return [operand1 * operand2, "multiply"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw`Unimplemented operator ${operator}. Aborting!`;
     }
+  
+    }
 
-
-}
 
 /**
  * Gets the current tally of incorrect answers from dom and increments by 1
@@ -112,8 +121,10 @@ function displaySubtractQuestion(){
 
 }
 
-function displayMultiplyQuestion(){
-
+function displayMultiplyQuestion(operand1, operand2){
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "x";
 }
 
 function displayDivisionQuestion(){
