a603da5ac05123f1dafc987154ce45506c33872e
PritenMakwana44 <priten.makwana7@gmail.com>
2022-08-09 14:00:34 +0000

    create random generated numbers in run game function

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,8 +16,15 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     }
 })
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed.
+ */
+function runGame() {
+    // Creates two random number between 1 and 25
 
-function runGame(){
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
 }
 
